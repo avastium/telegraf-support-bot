8ea8f002529fe0b9051d622713ca13e62c0cc928
Avastium <romamelnikov1@gmail.com>
2022-03-08 22:57:38 +0300

    some improvements

--- index.js
+++ index.js
@@ -4,30 +4,25 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 await mongoose.connect(process.env.DB).then().catch(e => console.log(e));
 
-const groupID = -1001234567890; // YOUR GROUP ID
-
 const db = mongoose.connection;
 const messageSchema = new mongoose.Schema({
   tg_id: {type: Number, default: 0},
   msg_id: {type: Number, default: 0},
   time: {type: Date, default: Date()}
-}, {autoCreate: false});
+});
 const Message = mongoose.model('Message', messageSchema);
 
 bot.start((ctx) => ctx.reply('Welcome to Support Bot.\nPlease ask your question and wait for the answer.'));
 
 bot.on('message', (ctx) => {
-  if (ctx.chat.id != groupID) {
-    ctx.telegram.forwardMessage(groupID, ctx.chat.id, ctx.message.message_id).then((res) => {
-      db.collection('support').insertOne(new Message({tg_id: ctx.chat.id, msg_id: res.message_id, time: Date.now()})).then().catch(e => console.log(e));
+  if (ctx.chat.id == process.env.GROUP_ID && reply_to_message in ctx.message) {
+    db.collection('messages').findOne({msg_id: ctx.message.reply_to_message.message_id}).then((res) => {
+      ctx.telegram.copyMessage(res.tg_id, process.env.GROUP_ID, ctx.message.message_id);
     }).catch(e => console.log(e));
-  }
-  if (ctx.chat.id == groupID) {
-    if ('reply_to_message' in ctx.message) {
-      db.collection('support').find({msg_id: ctx.message.reply_to_message.message_id}).toArray().then((res) => {
-        ctx.telegram.copyMessage(res[0]['tg_id'], groupID, ctx.message.message_id);
-      }).catch(e => console.log(e));
-    }
+  } else {
+    ctx.telegram.forwardMessage(process.env.GROUP_ID, ctx.chat.id, ctx.message.message_id).then((res) => {
+      db.collection('messages').insertOne(new Message({tg_id: ctx.chat.id, msg_id: res.message_id, time: Date.now()})).then().catch(e => console.log(e));
+    }).catch(e => console.log(e));
   }
 });
 
