bff553762ed3f403a11b1d327c33ff705cc04cae
Avastium <romamelnikov1@gmail.com>
2022-04-19 20:02:48 +0300

    use mongoose models

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 import { Telegraf } from 'telegraf';
 import mongoose from 'mongoose';
-const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const bot = new Telegraf(process.env.BOT_TOKEN);
 await mongoose.connect(process.env.DB).then().catch(e => console.log(e));
 
-const db = mongoose.connection;
 const messageSchema = new mongoose.Schema({
   tg_id: {type: Number, default: 0},
   msg_id: {type: Number, default: 0},
   time: {type: Date, default: Date()}
-});
+}, {versionKey: false});
 const Message = mongoose.model('Message', messageSchema);
 
 bot.start((ctx) => ctx.reply('Welcome to Support Bot.\nPlease ask your question and wait for the answer.'));
 
 bot.on('message', (ctx) => {
   if (ctx.chat.id == process.env.GROUP_ID && reply_to_message in ctx.message) {
-    db.collection('messages').findOne({msg_id: ctx.message.reply_to_message.message_id}).then((res) => {
-      ctx.telegram.copyMessage(res.tg_id, process.env.GROUP_ID, ctx.message.message_id);
+    Message.findOne({msg_id: ctx.message.reply_to_message.message_id}).then((res) => {
+      ctx.telegram.copyMessage(res.tg_id, process.env.GROUP_ID, ctx.message.message_id).then().catch(e => console.log(e));
     }).catch(e => console.log(e));
   } else {
     ctx.telegram.forwardMessage(process.env.GROUP_ID, ctx.chat.id, ctx.message.message_id).then((res) => {
-      db.collection('messages').insertOne(new Message({tg_id: ctx.chat.id, msg_id: res.message_id, time: Date.now()})).then().catch(e => console.log(e));
+      Message.create({tg_id: ctx.chat.id, msg_id: res.message_id, time: Date.now()}).then().catch(e => console.log(e));
     }).catch(e => console.log(e));
   }
 });
