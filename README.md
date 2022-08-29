3befb73cabef31b0daa23f2346f7e415376cf1b1
Avastium <romamelnikov1@gmail.com>
2022-08-29 14:49:35 +0300

    refactor

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Install
-* Create a bot, add a token to `process.env.BOT_TOKEN`.
-* Create a MongoDB database, add connection string to `process.env.DB`.
-* Create a group, add your bot to it.
-* Add a group ID to `process.env.GROUP_ID`.
+* Add your bot token to `process.env.BOT_TOKEN`.
+* Add your MongoDB connection string to `process.env.DB`.
+* Add your group ID to `process.env.GROUP_ID`.
+* Add your bot to your group.
 
 # Usage
 The bot will forward all user messages to the group.
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Telegraf } from 'telegraf';
 import mongoose from 'mongoose';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-await mongoose.connect(process.env.DB).then().catch(e => console.log(e));
+await mongoose.connect(process.env.DB);
 
 const messageSchema = new mongoose.Schema({
   tg_id: {type: Number, default: 0},
@@ -12,15 +12,15 @@ const Message = mongoose.model('Message', messageSchema);
 
 bot.start((ctx) => ctx.reply('Welcome to Support Bot.\nPlease ask your question and wait for the answer.'));
 
-bot.on('message', (ctx) => {
-  if (ctx.chat.id == process.env.GROUP_ID && reply_to_message in ctx.message) {
-    Message.findOne({msg_id: ctx.message.reply_to_message.message_id}).then((res) => {
-      ctx.telegram.copyMessage(res.tg_id, process.env.GROUP_ID, ctx.message.message_id).catch(e => console.log(e));
-    }).catch(e => console.log(e));
+bot.on('message', async (ctx) => {
+  if (ctx.chat.id == process.env.GROUP_ID) {
+    if ('reply_to_message' in ctx.message) {
+      const { tg_id } = await Message.findOne({msg_id: ctx.message.reply_to_message.message_id});
+      await ctx.telegram.copyMessage(tg_id, process.env.GROUP_ID, ctx.message.message_id).catch(e => console.log(e));
+    }
   } else {
-    ctx.telegram.forwardMessage(process.env.GROUP_ID, ctx.chat.id, ctx.message.message_id).then((res) => {
-      Message.create({tg_id: ctx.chat.id, msg_id: res.message_id}).catch(e => console.log(e));
-    }).catch(e => console.log(e));
+    const { message_id } = await ctx.telegram.forwardMessage(process.env.GROUP_ID, ctx.chat.id, ctx.message.message_id);
+    await Message.create({tg_id: ctx.chat.id, msg_id: message_id});
   }
 });
 
